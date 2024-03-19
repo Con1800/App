7220ad2a58e9c535414a72eb8fdf8fccaac0be6f
con1800 <rcon1800@gmail.com>
2024-03-19 21:21:39 +0800

    second commit march

--- src/pages/Ml.tsx
+++ src/pages/Ml.tsx
@@ -2,6 +2,7 @@ const Ml = () => {
   return (
     <div>
      <h1>machine learning topics here. <span className="bg-yellow-400">and research for M.L, A.I, Deep Learning</span> you can read the articles about this topics <span className="text-blue-400 cursor-pointer font-bold">here</span></h1>
+     <p className="text-pink-300 text-bold">put a sidebar</p>
     </div>
   )
 }
